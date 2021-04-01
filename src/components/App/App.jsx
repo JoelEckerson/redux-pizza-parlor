37f406f720b397f19157618014df602d39e8bb14
Joel Eckerson <joelaeckerson@gmail.com>
2021-04-01 14:50:33 -0500

    GET/POST/GET working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
 import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 import './App.css';
 
 function App() {
+
+  useEffect(()=>{
+      getMenu(),
+      getOrder()
+  }, [])
+
+  const [ newOrder, setNewOrder] = useState( [] );
+
+  let addOrder = () =>{
+    console.log('in addOrder:', newOrder );
+    axios.post('/api/order', newOrder ).then((response)=>{
+      console.log( 'back from addOrder POST with:', response);
+      getMenu();
+    }).catch((err)=>{
+      console.log(err);
+      alert('addOrder POST not working')
+    })
+  }
+
+  let getMenu = () =>{
+    console.log('in getMenu');
+    axios.get('/api/pizza').then((response) =>{
+      console.log('back from getMenu GET with:', response);
+      //dispatchEvent({type: 'setNewOrder', payload: response.data});
+    }).catch( (err) =>{
+      console.log(err);
+      alert('getMenu GET not working');
+    })
+  }
+
+  let getOrder = () =>{
+    console.log('in getOrder');
+    axios.get('/api/order').then((response) =>{
+      console.log('back from getOrder GET with:', response);
+      //dispatchEvent({type: 'setNewOrder', payload: response.data});
+    }).catch( (err) =>{
+      console.log(err);
+      alert('getOrder GET not working');
+    })
+  }
 
   return (
     <div className='App'>
@@ -12,6 +54,7 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
+      <button>post pizza</button>
   
     </div>
   );
